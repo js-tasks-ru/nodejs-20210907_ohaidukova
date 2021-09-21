bd2295f8029c203a75cba9f62ddd42dc56a325f1
OHaidukova <OHaidukova@rambler.ru>
2021-09-21 23:31:27 +0300

    add solution for 4-module 1-task

--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 
 describe('testing-configuration-logging/unit-tests', () => {
   describe('Validator', () => {
-    it('валидатор проверяет строковые поля', () => {
+    it('валидатор проверяет строковые поля: min длина', () => {
       const validator = new Validator({
         name: {
           type: 'string',
@@ -16,7 +16,161 @@ describe('testing-configuration-logging/unit-tests', () => {
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
-      expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
+      expect(errors[0])
+        .to.have.property('error')
+        .and.to.be.equal('too short, expect 10, got 6');
+    });
+
+    it('валидатор проверяет строковые поля: border min', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({ name: 'Lalalalala' });
+
+      expect(errors).to.have.length(0);
+    });
+
+    it('валидатор проверяет строковые поля: max длина', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({
+        name: 'Lalala lalala lalala lalala lalala',
+      });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0])
+        .to.have.property('error')
+        .and.to.be.equal('too long, expect 20, got 34');
+    });
+
+    it('валидатор проверяет строковые поля: border max', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({
+        name: 'Lalala lalala lalala',
+      });
+
+      expect(errors).to.have.length(0);
+    });
+
+    it('валидатор проверяет строковые поля: type', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({
+        name: 3,
+      });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0])
+        .to.have.property('error')
+        .and.to.be.equal('expect string, got number');
+    });
+
+    it('валидатор проверяет числовые поля: min значение', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({ age: 3 });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0])
+        .to.have.property('error')
+        .and.to.be.equal('too little, expect 10, got 3');
+    });
+
+    it('валидатор проверяет числовые поля: border min', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({ age: 10 });
+
+      expect(errors).to.have.length(0);
+    });
+
+    it('валидатор проверяет числовые поля: max значение', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({ age: 30 });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0])
+        .to.have.property('error')
+        .and.to.be.equal('too big, expect 20, got 30');
+    });
+
+    it('валидатор проверяет числовые поля: border max', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({ age: 20 });
+
+      expect(errors).to.have.length(0);
+    });
+
+    it('валидатор проверяет числовые поля: type', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({ age: '30' });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0])
+        .to.have.property('error')
+        .and.to.be.equal('expect number, got string');
     });
   });
 });
